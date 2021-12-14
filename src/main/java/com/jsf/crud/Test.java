dfdc4e9f1746f80b05e134679d8d680b6636d390
nkadyrov <n.kadyrov@infocom.kg>
2021-12-14 16:53:08 +0600

    Revert "- file changed"
    This reverts commit aed793aa

--- src/main/java/com/jsf/crud/Test.java
+++ src/main/java/com/jsf/crud/Test.java
@@ -1,8 +1,4 @@
 package com.jsf.crud;
 
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("sdfsdfsdf");
-        System.out.println("sdfsdfsdf");
-    }
 }
