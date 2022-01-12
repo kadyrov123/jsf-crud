0cfd6fc96422b26add3732bdc3b0d7c4197ab271
nkadyrov <n.kadyrov@infocom.kg>
2022-01-12 17:37:55 +0600

    - master hotfix  2

--- src/main/java/com/jsf/crud/SecondNewFile.java
+++ src/main/java/com/jsf/crud/SecondNewFile.java
@@ -2,6 +2,6 @@ package com.jsf.crud;
 
 public class SecondNewFile {
     public static void main(String[] args) {
-        System.out.println("master-hotfix-2");
+        System.out.println("master-hotfix");
     }
 }
